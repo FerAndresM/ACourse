44c6fbe1c8638d1162770357e43ae6377b8e9228
Fernando Mejia <Fernando.Mejia@ge.com>
2017-01-15 15:24:16 -0600

    While loop fixed

--- Fibonacci/src/FibonacciUI.java
+++ Fibonacci/src/FibonacciUI.java
@@ -15,7 +15,7 @@ public class FibonacciUI {
 		long input = 0, result;
 		boolean keepRunning = true;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while (keepRunning == true) 
+		while (keepRunning == true){
 			System.out.println("Enter Integer to Calculate Fibonacci value, or q to quit:");
 			String inputString;
 			try {
